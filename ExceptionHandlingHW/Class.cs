a1a37187f2583fe8e425faa61a1d95d3b4995a9f
Aspandiyar-Zhakyp <75847712+Aspandiyar-Zhakyp@users.noreply.github.com>
2021-02-13 17:02:16 +0600

    DZ5/ Aspandiyar-Zhakyp Исключения

---
+++ ExceptionHandlingHW/Class.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    public class Class
+    {
+        public int Argument(int a, int b)
+        {
+            int c;
+            // Handle null argument.
+            if (b == null & a == null)
+            {
+                throw new ArgumentNullException("параметры не заданы");
+            }
+            // Handle invalid argument.
+            if (b == 0)
+            {
+                throw new ArgumentException("нельзя делить на 0");
+            }
+            return c = a / b;
+        }
+    }
+}
---
+++ ExceptionHandlingHW/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.реализовать метод деления двух чисел. Внутри метода проверять входящие параметры.Если они не заданы, то генерировать исключение ArgumentNullException.Если 
+            //    делитель равен нулю, то генерировать исключение ArgumentException.
+
+            //2.Написать программу, которая обращается к элементам 
+            //    массива по индексу и выходит за его пределы.
+            //    После обработки исключения вывести
+            //    в финальном блоке сообщение о завершении обработки массива.
+            int[] array = new int[3];
+            try
+            {
+                array[4] = 4;
+                Console.WriteLine("нет такого индекса");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("завершение обработки массива");
+            }
+            Class class1 = new Class();
+            class1.Argument(2, 0); Console.ReadLine();
+        }
+    }
+}
